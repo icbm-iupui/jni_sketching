f44b082de8123a178b1307d9c7fab0856fe2d2e1
winfrees <winfrees@IN-MDEP-140187.ads.iu.edu>
2017-07-10 16:34:33 -0400

    confirmed operation on SPIM, updated library loads

--- src/physics/Physics.java
+++ src/physics/Physics.java
@@ -157,7 +157,8 @@ public class Physics {
         try{
             //System.load("C:\\Users\\andmcnut\\Documents\\NetBeansProjects\\jniPhysics\\src\\physics\\launcher.dll");
             //System.load("C:\\Users\\winfrees\\Documents\\NetBeansProjects\\jni_sketching\\src\\physics\\launcher.dll");
-            System.load("/Users/sethwinfree/NetBeansProjects/jni_sketching/lib/launcher.so");
+            //System.load("/Users/sethwinfree/NetBeansProjects/jni_sketching/lib/launcher.so");
+            System.load("C:\\Users\\winfrees\\Documents\\NetBeansProjects\\jni_sketching\\lib\\launcher.dll");
             Physics start = new Physics();
         }
        catch (NullPointerException e){
@@ -187,7 +188,8 @@ class Trajectory extends JPanel{
     
     static{
         //System.load("C:\\Users\\winfrees\\Documents\\NetBeansProjects\\jni_sketching\\src\\physics.dll");
-        System.load("/Users/sethwinfree/NetBeansProjects/jni_sketching/lib/physics.so");
+        //System.load("/Users/sethwinfree/NetBeansProjects/jni_sketching/lib/physics.so");
+        System.load("C:\\Users\\winfrees\\Documents\\NetBeansProjects\\jni_sketching\\lib\\physics.dll");
     }
     //private native double getXVel(int speed, double degrees);
     //private native double getYVel(int speed, double degrees);
